9e09ef14ab7d599d808ee5d2d52385ad290814c1
Jose Luiz Fagundes <164180216+joseluizfagundes@users.noreply.github.com>
2024-09-27 14:30:02 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,22 +1,7 @@
-// create web server with express
+// Create web server
+// Create a new express application
 const express = require('express');
 const app = express();
-const port = 3000;
 
-// import the comments data
-const comments = require('./data/comments');
-
-// import the posts data
-const posts = require('./data/posts');
-
-// import the likes data
-const likes = require('./data/likes');
-
-// import the messages data
-const messages = require('./data/messages');
-
-// import the users data
-const users = require('./data/users');
-
-// import the friends data
-const friends = require('./data/friends
+// Create a new comments array
+const comments = [];
